b3b02949f0ecc095f732818ac0f9df6b6cb57a44
Haresh Kotkar <75067419+harshkotkar5557@users.noreply.github.com>
2021-09-09 00:29:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const birthdate = document.getElementById('birthdate')
+const lucky_number = document.getElementById('lucky_number')
+const submit_button = document.getElementById('submit_button')
+const result_section = document.getElementById('result_section')
+
+
+birthdate.value
+submit_button.addEventListener('click' ,() =>{
+    let totalOfBirthDate = 0
+    for(let i=0 ; i< birthdate.value.toString().length;i++){
+        let eachValue=birthdate.value.toString().charAt(i) 
+        if(eachValue == "-"){
+            eachValue ="0"
+        }
+        let numValue = Number(eachValue)
+        totalOfBirthDate += numValue
+    }
+    let luckyNumber = Number(lucky_number.value)
+    if(!luckyNumber){
+        return result_section.innerHTML = "Invalid data"
+    }
+    if(totalOfBirthDate % luckyNumber ===0){
+            return result_section.innerHTML = `Your birthdate is lucky 🎂 `
+    }else{
+            return result_section.innerHTML = `Your birthdate is not lucky 😕`
+    }
+  
+})
