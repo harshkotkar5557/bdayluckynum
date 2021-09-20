da16ebe0ffc06b6a418195d1aa06f34c8ec25154
Haresh Kotkar <75067419+harshkotkar5557@users.noreply.github.com>
2021-09-20 23:19:24 +0530

    feat : added loading feature

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const birthdate = document.getElementById('birthdate')
 const lucky_number = document.getElementById('lucky_number')
 const submit_button = document.getElementById('submit_button')
 const result_section = document.getElementById('result_section')
+const img_loading = document.querySelector('.img_loading')
 
 
-birthdate.value
-submit_button.addEventListener('click' ,() =>{
-    let totalOfBirthDate = 0
+submit_button.addEventListener('click', () => {
+    img_loading.style.display = 'block'
+    result_section.innerHTML = ''
+    setTimeout(() => {
+        img_loading.style.display = 'none'
+        let totalOfBirthDate = 0
     for(let i=0 ; i< birthdate.value.toString().length;i++){
         let eachValue=birthdate.value.toString().charAt(i) 
         if(eachValue == "-"){
@@ -21,10 +25,14 @@ submit_button.addEventListener('click' ,() =>{
     }if(luckyNumber <= 0){
         return result_section.innerHTML = "Invalid data"
     }
-    if(totalOfBirthDate % luckyNumber ===0){
+        if (totalOfBirthDate % luckyNumber === 0) {
+            result_section.style.color = 'green'
             return result_section.innerHTML = `Your birthdate is lucky 🎂 `
-    }else{
+        } else {
+            result_section.style.color = 'green'
             return result_section.innerHTML = `Your birthdate is not lucky 😕`
     }
+    },1500)
+    
   
 })
